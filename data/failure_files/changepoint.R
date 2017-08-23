e0eb60e285c8ac31293e1c318c0009e95eae26c1
Softcheck <Sean.Softcheck1@centurylink.com>
2017-08-23 17:07:55 -0400

    changepoint and correlation code update

--- data/failure_files/changepoint.R
+++ data/failure_files/changepoint.R
@@ -7,6 +7,16 @@ library(lubridate)
 library(ggplot2)
 library(progress)
 
+readData = function(x) {
+  # filter days to save time/space
+  d=fread(x)
+  d[,timestamp:=ymd_hms(timestamp)]
+  failtime = min(d[fail==1,timestamp])
+  if(!is.na(failtime))
+    d = d[difftime(failtime,timestamp,units="hours") < 24]
+  return(d)
+}
+
 setwd("C:/Users/AB58342/Documents/device failures/data/failure_files")
 file.names = grep("^h\\_.+csv$",list.files(),value=TRUE)
 term.point = regexpr("^h\\_[A-Z]+",file.names,perl=TRUE)
@@ -14,15 +24,7 @@ term.point = regmatches(file.names,term.point)
 
 file.names = split(file.names,term.point)
 
-files = lapply(file.names,function(x) Reduce(function(a,b) rbind(a,b,fill=TRUE),Map(fread,x))[order(node_id,termination_point,timestamp)])
-
-for(i in seq(length(files))) {
-  files[[i]][,timestamp := ymd_hms(timestamp)]
-  # filter days to save time/space
-  failtime = min(files[[i]][fail==1,timestamp])
-  files[[i]] = files[[i]][difftime(failtime,timestamp,units="hours") < 48]
-  rm(failtime)
-}
+files = lapply(file.names,function(x) Reduce(function(a,b) rbind(a,b,fill=TRUE),Map(readData,x))[order(node_id,termination_point,timestamp)])
 
 varcheck = Map(function(x) melt(x[,lapply(.SD,function(z) c(miss=sum(is.na(z)),nomiss=sum(!is.na(z)))),by=node_id],id.vars="node_id"),files)
 varcheck = Map(function(x) x[,.(miss=value[1],nomiss=value[2]),by=.(node_id,variable)],varcheck)
@@ -38,50 +40,62 @@ pbar = progress_bar$new(total=length(files),format="[:bar] :current :percent :el
 cl = makeCluster(2)
 registerDoParallel(cl)
 
+pval.next = 0.05
 for(i in names(files)) {
   segments = unique(files[[i]][,.(node_id,termination_point)])
   
-  tests = foreach(x = iter(segments,by="row"),.packages=c("data.table","ecp"),.combine="list",.multicombine = TRUE) %dopar% {
+  tests = foreach(x = iter(segments,by="row"),.packages=c("data.table","ecp"),.multicombine=TRUE) %dopar% {
+    print(x)
     selection = with(files[[i]],node_id==x$node_id & termination_point==x$termination_point)
-    vlist = setdiff(names(files[[i]])[sapply(files[[i]][selection],class) %in% c("numeric","integer")],c("timestamp","fail","validity"))
+    vlist = setdiff(names(files[[i]])[sapply(files[[i]][selection],class) %in% c("numeric","integer")],c("timestamp","fail","validity","NumSampleSecs"))
     v = sapply(files[[i]][selection,vlist,with=FALSE],var)
     vlist = vlist[v > 0 & !is.na(v)]
-    result = e.divisive(as.matrix(files[[i]][selection,vlist,with=FALSE]),sig.lvl=0.005,R=200,alpha=1)
+    result = e.divisive(as.matrix(files[[i]][selection,vlist,with=FALSE]),sig.lvl=pval.next,R=200,alpha=1)
+    # result = e.agglo(X = as.matrix(files[[i]][selection,vlist,with=FALSE]),alpha = 1)
     result$vars = vlist
     result$name = paste(x,collapse = " ")
     return(result)
   }
+  # sep = which(names(tests) ==  "name")
+  # sep = cbind(c(1,sep[-length(sep)]+1),sep)
+  # tests = apply(sep,1,function(x) tests[x[1]:x[2]])
+  names(tests) = unlist(Map(function(x) x$name,tests))
 
-  names(tests) = sapply(tests,function(x) x$name)
   
-  plots = foreach(x = iter(segments,by="row"),.packages=c("data.table","ecp","ggplot2"),.combine="list",.multicombine = TRUE) %do% {
+  plots = foreach(x = iter(segments,by="row"),.packages=c("data.table","ggplot2"),.multicombine = TRUE) %do% {
     label = paste(x,collapse=" ")
     selection = with(files[[i]],node_id==x$node_id & termination_point==x$termination_point)
     if(label %in% names(tests)) {
-      result = lapply(tests[[label]]$vars,function(y) {
-        ds = files[[i]][selection,c(y,"fail","timestamp"),with=FALSE]
+      result = vector("list",length(tests[[label]]$vars))
+      names(result) = tests[[label]]$vars
+      for(y in tests[[label]]$vars) {
+        ds = files[[i]][selection,c(y,"chassis_id","node_id","termination_point","fail","timestamp"),with=FALSE]
         ds[,cp:= 0]
         ds[tests[[label]]$estimates[c(-1,-length(tests[[label]]$estimates))],cp := 1]
         failure = as.numeric(ds[fail==1][["timestamp"]])
         cp = as.numeric(ds[cp==1][["timestamp"]])
-        setnames(ds,c("timestamp",y),c("Time","val"))
-        p = ggplot(ds) + aes(x=Time,y=val) + geom_line() + ylab(y) + ggtitle(paste(i,gsub(" ","\n",label),sep="\n")) +
-          geom_vline(xintercept = failure,color="red",size=1.5) +
-          geom_vline(xintercept = cp,color="blue",size=1)
-        return(p)
-      })
-      names(result) = tests[[label]]$vars
+
+        p = ggplot(ds) + aes_string(x="timestamp",y=y) + geom_line() + ylab(y) + xlab("Time") + ggtitle(paste(i,gsub(" ","\n",label),sep="\n")) +
+          geom_vline(xintercept = failure,color="red",size=1.5) + geom_vline(xintercept = cp,color="blue",size=1)
+
+        result[[y]] = list(plot=p,data=ds)
+      }
       return(result)
     }
     else
       return(NULL)
   }
-  rm(label,selection,result,x)
-
   names(plots) = names(tests)
-  pdf(paste0(i,".pdf"))
-  print(plots)
+  rm(label,selection,result,x,y,ds,p,failure,cp)
+
+  
+  pdf(paste0(i,"_changepoint_plots.pdf"))
+  Map(function(x) Map(function(y) print(y$plot),x),plots)
   dev.off()
+  
+  # data output
+  out.data = Reduce(function(a,b) rbind(a,b,fill=TRUE),Map(function(x) Reduce(function(a,b) merge(a,b[,],by=c("timestamp","chassis_id","node_id","termination_point","fail","cp")),Map(function(y) y$data,x)),plots))
+  fwrite(out.data,paste0(i,"_changepoint_data.csv"))
   
   pbar$tick()
 }
--- data/failure_files/correlation.R
+++ data/failure_files/correlation.R
@@ -5,73 +5,68 @@ library(lubridate)
 library(ggplot2)
 library(progress)
 
-setwd("C:/Users/AB58342/Documents/device failures/data/failure_files")
-file.names = grep("^h\\_.+csv$",list.files(),value=TRUE)
-term.point = regexpr("^h\\_[A-Z]+",file.names,perl=TRUE)
-term.point = regmatches(file.names,term.point)
+setwd("C:/Users/AB58342/Documents/device failures/data")
+file.names = grep("^h\\_",list.files(),value=TRUE)
 
-file.names = split(file.names,term.point)
+files = lapply(file.names,function(x) list.files(paste(getwd(),x,sep="/"),full.names = TRUE))
+names(files) = file.names
+files = files[sapply(files,length) > 0]
 
-files = lapply(file.names,function(x) Reduce(function(a,b) rbind(a,b,fill=TRUE),Map(fread,x))[order(node_id,termination_point,timestamp)])
-
-for(i in seq(length(files))) {
-  files[[i]][,timestamp := ymd_hms(timestamp)]
+readData = function(x) {
   # filter days to save time/space
-  # failtime = min(files[[i]][fail==1,timestamp])
-  # files[[i]] = files[[i]][difftime(failtime,timestamp,units="hours") < 48]
-  # rm(failtime)
+  d=fread(x)
+  d[,timestamp:=ymd_hms(timestamp)]
+  # failtime = min(d[fail==1,timestamp])
+  # if(!is.na(failtime))
+  #   d = d[difftime(failtime,timestamp,units="hours") < 24*3]
+  return(d)
 }
 
-varlist = fread("corr vars.csv",stringsAsFactors = FALSE)
-
+pbar = progress_bar$new(total=length(files),format="[:bar] :current :percent :elapsed")
 cormat = vector("list",length(files))
 names(cormat) = names(files)
 
-pbar = progress_bar$new(total=length(files),format="[:bar] :current :percent :elapsed")
-for(i in names(cormat)) {
-  cormat[[i]] = cor(as.matrix(files[[i]][,c("fail",varlist[h==i,variable]),with=FALSE]),method="pearson",use="pairwise.complete.obs")
-  pbar$tick()
-}
-
-melted.cormat = lapply(cormat,reshape2::melt)
-
-for(i in names(melted.cormat)) {
-  p = ggplot(data = melted.cormat[[i]]) + theme(axis.text=element_text(size=14),
-                                                title = element_text(size=20,face="bold"),
-                                                legend.title = element_text(size=14)) +
-    aes(x=Var1, y=Var2, fill=value) + geom_tile() + xlab("") + ylab("") + ggtitle(i) + scale_fill_gradient2(name="Correlation\n")
-  ggsave(p,filename = paste0(i,".png"),device = "png",width = 40 ,height = 25 ,dpi = 400)
-}
-
-
-# try by terminatin point block
-cormat = vector("list",length(files))
-names(cormat) = names(files)
-pbar = progress_bar$new(total=length(files),format="[:bar] :current :percent :elapsed")
-for(i in names(cormat)) {
-  segments = unique(files[[i]]$termination_point)
+for(f in names(files)) {
+  ds = Reduce(function(a,b) rbind(a,b,fill=TRUE),Map(readData,files[[f]]))[order(node_id,termination_point,timestamp)]
+  
+  #combine segments
+  ds[,node_id := "ALL"]
+  
+  segments = unique(ds$node_id)
   cormat[[i]] = vector("list",length(segments))
   names(cormat[[i]]) = segments
   
-  pdf(paste0(i,"_corr_matrix",".pdf"),width=40,height = 25)
+  pdf(paste0("./failure_files/corrplots/",f,"_corr_matrix.pdf"),width=40,height = 25)
   for(j in segments) {
-    vlist = setdiff(names(files[[i]])[sapply(files[[i]][termination_point==j],class) %in% c("numeric","integer")],c("termination_point","node_ctrl","timestamp","fail","validity",
-                                                                                              grep("card(sub)?type|mode$|type$|id$|secs$|qualifier$",names(files[[i]]),value=TRUE,ignore.case = TRUE,perl=TRUE)))
-    v = sapply(files[[i]][termination_point==j,vlist,with=FALSE],var)
+    vlist = setdiff(names(ds)[sapply(ds[node_id==j],class) %in% c("numeric","integer")],c("termination_point","node_ctrl","timestamp","fail","validity",
+                                                                                                                  grep("card(sub)?type|mode$|type$|id$|secs$|qualifier$",names(ds),value=TRUE,ignore.case = TRUE,perl=TRUE)))
+    v = sapply(ds[node_id==j,vlist,with=FALSE],var,na.rm=TRUE)
     vlist = vlist[v > 0 & !is.na(v)]
+    
+    matches = "PMDAVE|RxEdfaOprAve|RxEdfaLbcAve|ChanOchChromaticDispersionMin|ChanOchChromaticDispersionAve"
+    matches = grep(matches,vlist,ignore.case = TRUE,perl=TRUE,value=TRUE)
+    if(length(matches)>0)
+      cat(f,": ",paste(matches,collapse="|"),"\n")
+    
     if(length(vlist) > 0) {
-      cormat[[i]][[j]] = cor(as.matrix(files[[i]][termination_point==j,c("fail",vlist),with=FALSE]),method="pearson",use="pairwise.complete.obs")
+      cormat[[i]][[j]] = cor(as.matrix(ds[node_id==j,c("fail",vlist),with=FALSE]),method="pearson",use="pairwise.complete.obs")
       melted.cormat = reshape2::melt(cormat[[i]][[j]])
-      p = ggplot(data = melted.cormat) + theme(axis.text=element_text(size=14),
-                                                    title = element_text(size=20,face="bold"),
-                                                    legend.title = element_text(size=14)) +
+      p = ggplot(melted.cormat) + theme(axis.text=element_text(size=14),
+                                               title = element_text(size=20,face="bold"),
+                                               legend.title = element_text(size=14)) +
         aes(x=Var1, y=Var2, fill=value) + geom_tile() + xlab("") + ylab("") + ggtitle(paste(i,j,sep="\n")) + scale_fill_gradient2(name="Correlation\n")
       #ggsave(p,filename = paste0(i,".png"),path = "corrplots/",device = "png",width = 40 ,height = 25 ,dpi = 300)
       print(p)
     }
   }
   dev.off()
+  
+  write.csv(as.data.frame(cormat[[i]][[j]]),paste0("./failure_files/corrplots/",f,"_corr_matrix.csv"))
+  rm(ds)
+  
   pbar$tick()
 }
+
+
 
 
