4e9c8f5f5ada996758a06ad073fd79f58d79887b
Sean Softcheck <ssoftcheck@gmail.com>
2017-09-21 15:13:54 -0700

    setup file update

---
+++ setup_changepoint.R
@@ -0,0 +1,26 @@
+termination_point = "h_OCHCTP"
+file_location = "C:/"
+
+library(foreach)
+library(doSNOW)
+library(parallel)
+library(ecp)
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(progress)
+library(tcltk)
+library(iterators)
+
+readData = function(x,filterTime=-1) {
+  # filter days to save time/space
+  d=fread(x)
+  d[,timestamp:=lubridate::ymd_hms(timestamp)]
+  d = d[order(termination_point,timestamp)]
+  if(filterTime > 0) {
+    failtime = min(d[fail==1,timestamp])
+    if(!is.na(failtime))
+      d = d[difftime(failtime,timestamp,units="hours") < filterTime]
+  }
+  return(d)
+}
